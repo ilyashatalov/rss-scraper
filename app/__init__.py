8914e013fc7b332e7b6f8e60ba9bdf3b1aa4d15c
Ilya Shatalov <Ilya.Shatalov@gmail.com>
2023-03-10 17:20:31 +0400

    feat(app): add import of views and models to init file

--- app/__init__.py
+++ app/__init__.py
@@ -24,3 +24,13 @@ app.config['SWAGGER'] = {
 }
 swagger = Swagger(app)
 db = SQLAlchemy(app)
+
+from app.models.Feed import Feed
+from app.models.Item import Item
+
+with app.app_context():
+    db.create_all()
+
+from app.views.feeds import get_all_feeds, get_feed_items, follow_feed, unfollow_feed, update_feed, \
+    force_all_feeds_update, force_feed_update
+from app.views.items import set_read, get_all_items
